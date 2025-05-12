281c66e3ab810f707bf7b0e2ee30e83c89aee7b5
TejaswitaSingh <tejaswitasinghtanwar@gmail.com>
2025-05-12 18:20:53 +0530

    implemented registeration api integration

---
+++ src/services/apiService.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const API_BASE_URL = 'http://localhost:5000';
+const ADMIN_URL = '/admin';
+
+class apiService{
+    //admin
+    static async Register(formData){
+        return axios.post(`${API_BASE_URL}${ADMIN_URL}/register`,formData);
+    }
+}
+
+export default apiService;
--- src/views/authentication/auth/AuthRegister.jsx
+++ src/views/authentication/auth/AuthRegister.jsx
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Box, Typography, Button } from '@mui/material';
 import { Link } from 'react-router';
+import apiService from '../../../services/apiService';
 
 import CustomTextField from '../../../components/forms/theme-elements/CustomTextField'
 
 import { Stack } from '@mui/system';
+import { LineAxisOutlined } from '@mui/icons-material';
 
-const AuthRegister = ({ title, subtitle, subtext }) => (
+const AuthRegister = ({ title, subtitle, subtext }) =>{
+
+    const [formData,setFormData]=useState({
+        firstName:"",
+        lastName:"",
+        email:"",
+        password:""
+    });
+
+    const handleChange=(e)=>{
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+    }
+
+    const handleSubmit = async(e)=>{
+        e.preventDefault();
+        try{
+            const response = await apiService.Register(formData);
+            console.log('Registration successful:', response.data);
+        }catch (error) {
+        console.error('Registration failed:', error);
+        }
+    }
+};
+    (
     <>
         {title ? (
             <Typography fontWeight="700" variant="h2" mb={1}>
@@ -16,21 +41,25 @@ const AuthRegister = ({ title, subtitle, subtext }) => (
 
         {subtext}
 
-        <Box>
+        <Box component="form" onSubmit={handleSubmit}>
             <Stack mb={3}>
                 <Typography variant="subtitle1"
-                    fontWeight={600} component="label" htmlFor='name' mb="5px">Name</Typography>
-                <CustomTextField id="name" variant="outlined" fullWidth />
+                    fontWeight={600} component="label" htmlFor='name' mb="5px">First Name</Typography>
+                <CustomTextField id="name" name="firstName" variant="outlined" fullWidth value={formData.firstName} onchange={handleChange} required/>
+
+                <Typography variant="subtitle1"
+                    fontWeight={600} component="label" htmlFor='name' mb="5px">Last Name</Typography>
+                <CustomTextField id="name" name="lastName" variant="outlined" fullWidth value={formData.lastName} onchange={handleChange}/>
 
                 <Typography variant="subtitle1"
                     fontWeight={600} component="label" htmlFor='email' mb="5px" mt="25px">Email Address</Typography>
-                <CustomTextField id="email" variant="outlined" fullWidth />
+                <CustomTextField id="email" name="email" variant="outlined" fullWidth value={formData.email} onchange={handleChange} />
 
                 <Typography variant="subtitle1"
                     fontWeight={600} component="label" htmlFor='password' mb="5px" mt="25px">Password</Typography>
-                <CustomTextField id="password" variant="outlined" fullWidth />
+                <CustomTextField id="password" name="password" variant="outlined" fullWidth value={formData.password} onchange={handleChange} />
             </Stack>
-            <Button color="primary" variant="contained" size="large" fullWidth component={Link} to="/auth/login">
+            <Button type="submit" color="primary" variant="contained" size="large" fullWidth component={Link} to="/auth/login">
                 Sign Up
             </Button>
         </Box>
